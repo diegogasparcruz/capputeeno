c1bdb7829e3cffe9d2c81ee632fdc31a16bc9285
Diego Gaspar <diegogaspardacruz1@gmail.com>
2023-11-06 13:48:20 -0300

    chore: add api mock

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      boxShadow: {
+        'sort-by': '0px 4px 12px 0px rgba(0, 0, 0, 0.10)',
       },
     },
   },
