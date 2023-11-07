3f232357884561dbbb8d85689d5d565c10979138
Diego Gaspar <diegogaspardacruz1@gmail.com>
2023-11-06 23:31:42 -0300

    chore: config domain url images

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Saira, Saira_Stencil_One } from 'next/font/google'
+
+export const saira = Saira({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const sairaStencilOne = Saira_Stencil_One({
+  subsets: ['latin'],
+  weight: ['400'],
+})
